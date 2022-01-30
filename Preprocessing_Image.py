d8b917fe4eb4b22fa1bc16d91dc7993b9948146d
nmitic96 <98121621+nmitic96@users.noreply.github.com>
2022-01-30 21:10:30 +0100

    Update Preprocessing_Image.py

--- Preprocessing_Image.py
+++ Preprocessing_Image.py
@@ -1,11 +1,6 @@
 import cv2
 import pytesseract
 import re
-from matplotlib import pyplot as plt
-
-
-# Metoda za prikaz slike
-from numpy import angle
 from pytesseract import Output
 
 
@@ -57,55 +52,40 @@ class PreprocessingImage:
         #self.window = window
         """
 
-
-    def display(self, im_path):
-        dpi = 80
-        im_data = plt.imread(im_path)
-
-        height, width = im_data.shape[:2]
-
-        # What size does the figure need to be in inches to fit the image?
-        figsize = width / float(dpi), height / float(dpi)
-
-        # Create a figure of the right size with one axes that takes up the full figure
-        fig = plt.figure(figsize=figsize)
-        ax = fig.add_axes([0, 0, 1, 1])
-
-        # Hide spines, ticks, etc.
-        ax.axis('off')
-
-        # Display the image.
-        ax.imshow(im_data, cmap='gray')
-
-        plt.show()
-
-# Menja crne piksele u bele i obrnuto
+    # Menja crne piksele u bele i obrnuto
     def invert(self, image):
         inverted_image = cv2.bitwise_not(image)
         return inverted_image
 
-# Binarizacija
+    # Binarizacija
     def grayscale(self, image):
         return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#!!!!!!!!!!!!!!!!! komandu ispod uraditi posle poziva grayscale
-# thresh, im_bw = cv2.threshold(gray_image, 210, 230, cv2.THRESH_BINARY)
+    # !!!!!!!!!!!!!!!!! komandu ispod uraditi posle poziva grayscale
+    # thresh, im_bw = cv2.threshold(gray_image, 210, 230, cv2.THRESH_BINARY)
 
-# Uklanjanje šumova sa slike
-    def noise_removal(self, image):
+    # Stanjivanje fonta ( šalje mu se slika bez šumova )
+    def thin_font(self, image):
         import numpy as np
-        kernel = np.ones((1, 1), np.uint8)
-        image = cv2.dilate(image, kernel, iterations=1)
-        kernel = np.ones((1, 1), np.uint8)
+        image = cv2.bitwise_not(image)
+        kernel = np.ones((2, 2), np.uint8)
         image = cv2.erode(image, kernel, iterations=1)
-        image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
-        image = cv2.medianBlur(image, 3)
-        return (image)
+        image = cv2.bitwise_not(image)
+        return image
+
+    # Podebljanje fontova ( šalje mu se slika bez šumova )
+    def thick_font(self, image):
+        import numpy as np
+        image = cv2.bitwise_not(image)
+        kernel = np.ones((2, 2), np.uint8)
+        image = cv2.dilate(image, kernel, iterations=1)
+        image = cv2.bitwise_not(image)
+        return image
 
     def deskew(self, cvImage):
         angle = getSkewAngle(cvImage)
         return rotateImage(cvImage, -1.0 * angle)
 
-    # Uklanjanje okvira slike ako postoje ( i ovde se šalje slika bez šumova )
+    # Uklanjanje okvira slike ako postoje
     def remove_borders(self, image):
         contours, heiarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cntsSorted = sorted(contours, key=lambda x: cv2.contourArea(x))
@@ -114,35 +94,14 @@ class PreprocessingImage:
         crop = image[y:y + h, x:x + w]
         return (crop)
 
-    # Postavljanje okvira oko svakog karaktera
-    def create_borders_char(self,image):
-        h, w, c = image.shape
-        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-        boxes = pytesseract.image_to_boxes(image)
-        for b in boxes.splitlines():
-            b = b.split(' ')
-            image = cv2.rectangle(image, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
-        return image
-
-    # Postavljanje okvira oko svake reči
-    # ne radi baš najbolje
-    def create_borders_word(self,image):
-        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-        d = pytesseract.image_to_data(image, output_type=Output.DICT)
-        n_boxes = len(d['text'])
-        for i in range(n_boxes):
-            if int(d['conf'][i]) > 60:
-                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-                image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        return image
-
     # Nađi ukupan iznos na obrascu fakture
-    def find_total(self,image):
+    def find_total(self, image):
         pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
         d = pytesseract.image_to_data(image, output_type=Output.DICT)
         n_boxes = len(d['text'])
         for i in range(n_boxes):
             if re.match('Total', d['text'][i]):
-                (x, y, w, h) = (d['left'][i+1], d['top'][i+1], d['width'][i+1], d['height'][i+1])
+                (x, y, w, h) = (d['left'][i + 1], d['top'][i + 1], d['width'][i + 1], d['height'][i + 1])
                 image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        return image
+                return d['text'][i + 1]
+
